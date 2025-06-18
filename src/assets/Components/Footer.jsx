020e39ea859ded28ce3559bede65783537bf0d8b
abhijith1011 <mabhijith4243@gmail.com>
2025-06-18 20:53:34 +0530

    Footer done

---
+++ src/assets/Components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='bg-gray-200 text-center'>
+        <h1 className='text-[14px] p-10'>Website registered to Abhishek Reddy and Team <br></br>
+Copyright © 2025 Abhishek</h1>
+    </div>
+  )
+}
+
+export default Footer
